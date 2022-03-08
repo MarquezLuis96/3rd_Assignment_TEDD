07cbcf25772dfe2706a68e4e7e02248ba3b2cf1b
Luis Marquez <englamc@gmail.com>
2022-03-08 14:35:31 -0400

    Solution to read and write linked lists into bin files

--- DoublyLinkedList/Source.cpp
+++ DoublyLinkedList/Source.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+
+int const posiciones_maximas = 50;
 
 using namespace std;
 
@@ -41,19 +44,24 @@ public:
 template <class dataType>
 class DoubleLinkedList {
 private:
+	int nElements;
 	Node<dataType>* sentinel;
 public:
 	DoubleLinkedList() {
 		sentinel = new Node<dataType>;
 		sentinel->setNext(sentinel);
 		sentinel->setPrevious(sentinel);
+		nElements = 0;
 	}
+
+	int getNElements() { return nElements; }
 
 	void addNode(Node<dataType>* toAdd) {
 		toAdd->setNext(sentinel->getNext());
 		sentinel->getNext()->setPrevious(toAdd);
 		sentinel->setNext(toAdd);
 		toAdd->setPrevious(sentinel);
+		nElements++;
 	}
 
 	Node<dataType>* getSentinel() {
@@ -121,7 +129,134 @@ public:
 
 };
 
+struct Curso {
+	public:
+	//private:
+		int codigoCurso;
+		char nombre[posiciones_maximas];
+		char instructor[posiciones_maximas];
+	
+	//public:
+		//
+};
+
+struct Estudiante {
+	public:
+		int id;
+		char nombre[posiciones_maximas];
+		char apellido[posiciones_maximas];
+		DoubleLinkedList<Curso> listaCursos;
+	//public:
+		//
+};
+
 int main(int args, char* argsv[]) {
+	
+	/*Curso c1, c2, c3, c4, c5;
+	string str;
+
+	c1.codigoCurso = 1;
+	str = "Estructuras de Datos";
+	strcpy_s(c1.nombre, str.c_str());
+	str = "Gabriela Veracierta";
+	strcpy_s(c1.instructor, str.c_str());
+
+	c2.codigoCurso = 2;
+	str = "Taller de Estructuras de Datos";
+	strcpy_s(c2.nombre, str.c_str());
+	str = "Pedro Dorta";
+	strcpy_s(c2.instructor, str.c_str());
+
+	c3.codigoCurso = 3;
+	str = "Sistemas de Microprocesamiento";
+	strcpy_s(c3.nombre, str.c_str());
+	str = "Julima Anato";
+	strcpy_s(c3.instructor, str.c_str());
+
+	c4.codigoCurso = 4;
+	str = "Laboratorio de Disenio Logico";
+	strcpy_s(c4.nombre, str.c_str());
+	str = "Julima Anato";
+	strcpy_s(c4.instructor, str.c_str());
+
+	c5.codigoCurso = 5;
+	str = "Seguridad en Sistemas Computacionales";
+	strcpy_s(c5.nombre, str.c_str());
+	str = "Pedro Dorta";
+	strcpy_s(c5.instructor, str.c_str());
+	
+	Curso arrayCursos[] = { c1, c2, c3, c4, c5 };
+
+	Node<Curso>* auxCurso = nullptr;
+	Estudiante luisito;
+	int cont = 0;
+
+	while (cont < 5) {
+
+		auxCurso = new Node<Curso>;
+		auxCurso->setKey(arrayCursos[cont]);
+
+		luisito.listaCursos.addNode(auxCurso);
+
+		cont++;
+	}
+
+	luisito.id = 25687494;
+	str = "Luis Marquez";
+	strcpy_s(luisito.nombre, str.c_str());
+	str = "Marquez Castro";
+	strcpy_s(luisito.apellido, str.c_str());
+
+	fstream archivo("try.bin", ios::binary | ios::out);
+
+	archivo.write(reinterpret_cast<char*> (&luisito), sizeof(Estudiante));
+	
+	int n = luisito.listaCursos.getNElements();
+	
+	archivo.write(reinterpret_cast<char*> (&n), sizeof(int));
+
+	Node<Curso>* sentinela = luisito.listaCursos.getSentinel();
+	Node<Curso>* storeCurse = luisito.listaCursos.getSentinel()->getPrevious();
+
+	while (storeCurse != sentinela) {
+		Curso store = storeCurse->getKey();
+		archivo.write(reinterpret_cast<char*> (&store), sizeof(Curso));
+		
+		storeCurse = storeCurse->getPrevious();
+	}
+
+	archivo.close();*/
+	
+	Node<Curso>* storeCurse;
+	Estudiante estudianteLeido;
+	storeCurse = NULL;
+
+	
+	fstream archivo("try.bin", ios::binary | ios::in);
+	//archivo.open("try.bin", ios::binary | ios::in);
+	archivo.read(reinterpret_cast<char*> (&estudianteLeido), sizeof(Estudiante));
+	int cantReg;
+	archivo.read(reinterpret_cast<char*> (&cantReg), sizeof(int));
+	
+	Curso cursoleido;
+
+	DoubleLinkedList<Curso> listaauxiliar;
+
+	while (cantReg > 0) {
+		archivo.read(reinterpret_cast<char*> (&cursoleido), sizeof(Curso));
+		storeCurse = new Node<Curso>;
+		storeCurse->setKey(cursoleido);
+		listaauxiliar.addNode(storeCurse);
+		cantReg--;
+	}
+
+	estudianteLeido.listaCursos = listaauxiliar;
+
+	estudianteLeido;
+	//delete auxCurso;
+	delete storeCurse;
+
+	archivo.close();
 
 	return 0;
 }
