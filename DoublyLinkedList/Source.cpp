598f7e33803d58c699fd2b711c1dab3feaf9be53
Luis Marquez <englamc@gmail.com>
2022-03-16 13:11:29 -0400

    Clase Lista Doble

--- DoublyLinkedList/Source.cpp
+++ DoublyLinkedList/Source.cpp
@@ -4,6 +4,120 @@
 //DECLARANDO NOMBRES DE ESPACIO
 using namespace std;
 
+//CLASE NODO
+template <class dataType>
+struct Node {
+private:
+	Node* previous;
+	Node* next;
+	dataType key;
+public:
+	Node() {
+		previous = next = NULL;
+	}
+
+	void setKey(dataType newKey) {
+		key = newKey;
+	}
+
+	dataType getKey() {
+		return key;
+	}
+
+	void setPrevious(Node<dataType>* previousNode) {
+		previous = previousNode;
+	}
+
+	Node<dataType>* getPrevious() {
+		return previous;
+	}
+
+	void setNext(Node<dataType>* nextNode) {
+		next = nextNode;
+	}
+
+	Node<dataType>* getNext() {
+		return next;
+	}
+};
+
+//CLASE LISTA DOBLE
+template <class dataType>
+class DoubleLinkedList {
+private:
+	int nElements;
+	Node<dataType>* sentinel;
+
+public:
+
+	DoubleLinkedList() {
+		sentinel = new Node<dataType>;
+		sentinel->setNext(sentinel);
+		sentinel->setPrevious(sentinel);
+		nElements = 0;
+	}
+
+	void incrementNElements() {
+		nElements += 1;
+	}
+
+	int getNElements() {
+		return nElements; 
+	}
+
+	void addNode(Node<dataType>* toAdd) {
+		toAdd->setNext(sentinel->getNext());
+		sentinel->getNext()->setPrevious(toAdd);
+		sentinel->setNext(toAdd);
+		toAdd->setPrevious(sentinel);
+		nElements = nElements + 1;
+	}
+
+	Node<dataType>* getSentinel() {
+		return sentinel;
+	}
+
+	void printListInverse() {
+		int i = 0;
+		Node<dataType>* currentNode = sentinel->getNext();
+		while (currentNode != sentinel) {
+			cout << "N" << ++i << " = " << currentNode->getKey() << endl;
+			currentNode = currentNode->getNext();
+		}
+
+	}
+
+	void printListOrdered() {
+		int i = 0;
+		Node<dataType>* currentNode = sentinel->getPrevious();
+		while (currentNode != sentinel) {
+			cout << "N" << ++i << " = " << currentNode->getKey() << endl;
+			currentNode = currentNode->getPrevious();
+		}
+
+	}
+
+	void printListOrderedInline() {
+		int i = 0;
+		Node<dataType>* currentNode = sentinel->getPrevious();
+		while (currentNode != sentinel) {
+			cout << currentNode->getKey();
+			currentNode = currentNode->getPrevious();
+		}
+		cout << endl;
+	}
+
+	void agregarNodo(int i) {
+		struct Node<int>* p;
+		p = (Node<int>*) malloc(sizeof(struct Node<int>));
+		p->setKey(i);
+		p->setNext(sentinel);
+		p->setPrevious(sentinel->getPrevious());
+		sentinel->getPrevious()->setNext(p);
+		sentinel->setPrevious(p);
+	}
+};
+
 //FUNCION PRINCIPAL
 int main(int args, char* argsv[]) {
 	
